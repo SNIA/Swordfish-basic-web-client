6c51c2b9ab20b175496b3687ac0099c0fa607752
sravanthi kalluri <33854700+sravanthikalluri19@users.noreply.github.com>
2017-12-07 12:36:51 +0530

    Update home.component.ts

--- views/src/app/home/home.component.ts
+++ views/src/app/home/home.component.ts
@@ -1,6 +1,4 @@
-/**
- * Created by Sravanthi on 4/24/2017.
- */
+
 
 import {Component, ViewChild, ViewContainerRef, ComponentFactoryResolver, ElementRef} from '@angular/core';
 import {Router} from '@angular/router';
