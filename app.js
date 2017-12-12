f18d59f4335e5af4d0b65d10fa96fe3a23894d74
sravanthikalluri19 <33854700+sravanthikalluri19@users.noreply.github.com>
2017-12-12 14:11:37 +0530

    latest updated files 12_12_2017
    Files updated

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
+/*
+ Copyright (c) 2017, The Storage Networking Industry Association.
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ Redistributions of source code must retain the above copyright notice,
+ this list of conditions and the following disclaimer.
+ Redistributions in binary form must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation
+ and/or other materials provided with the distribution.
+ Neither the name of The Storage Networking Industry Association (SNIA) nor
+ the names of its contributors may be used to endorse or promote products
+ derived from this software without specific prior written permission.
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ THE POSSIBILITY OF SUCH DAMAGE.
+ */
 'use strict'
 
     const bodyParser = require('body-parser');
     const express = require('express');
-    const fs = require('fs');
     const app = express();
 
     var cors = require('cors');
@@ -28,8 +51,9 @@
         next();
     });
 
+
 	app.get('/getCollectionData', function(req, response){
-		var urlString = req.query.Ip;
+        var urlString = req.query.Ip;
 		 request({
             headers: {
                 'X-Auth-Token':req.get('X-Auth-Token'),
@@ -38,7 +62,7 @@
             uri: urlString,
             method: 'GET'
         }, function (err, res, body) {
-              try {
+             try {
                  return response.send(JSON.parse(body));
              } catch (e) {
                 return response.send({status:404});
@@ -54,6 +78,10 @@
                 'Cookie':req.get('Cookie-Headers')
             },
         }, function (err, res) {
+            if(err) {
+                console.log(err);
+                return err;
+            }
             return response.send(res);
         });
 
